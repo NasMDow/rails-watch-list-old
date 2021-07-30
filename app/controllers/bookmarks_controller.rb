9c4d14e736de58672431cf056e48444ce998b31c
Nasreen Dowlut <nasmdow@gmail.com>
2021-07-30 13:11:25 +0100

    watch list challenge

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+# BookmarksController
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(params_bookmark)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully destroyed.'
+  end
+
+  private
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# lists (of movies)
+root to: 'lists#index'
+resources :lists, except: [:edit, :update] do
+  resources :bookmarks, only: [:new, :create]
+end
+
+#get 'lists', to: 'lists#index', as: :lists
+#post 'lists', to: 'lists#create'
+#get 'lists/new', to: 'lists#new', as: :new_list
+#get 'lists/:id', to: 'lists#show', as: :list
+#get 'list/:id', to: 'lists#destroy', as: :destroy_list
+
+# bookmarks (list of comments for a movie)
+#get 'bookmarks/new/lists/:id/movies/:id', to: 'bookmarks#new', as: :new_bookmark
+#post 'bookmarks/list/:id/movies/:id', to: 'bookmarks#create'
+delete 'bookmarks/lists/id:', to: 'bookmarks#destroy', as: :destroy_bookmark
 end
